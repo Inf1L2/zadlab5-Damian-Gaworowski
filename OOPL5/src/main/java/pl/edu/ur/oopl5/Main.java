5cd6444203e7d40b80f33b1e7c3701c33894db9b
HP <HP@DESKTOP-LSMEN80>
2018-04-07 19:18:58 +0200

    12.03.2018

--- OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Main.java
@@ -1,9 +1,45 @@
 package pl.edu.ur.oopl5;
 
-public class Main {
 
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        // TODO zad 3, 5, 6
+
+        int x;
+        int y;
+        Scanner odczyt = new Scanner(System.in);
+        System.out.println("Jaka jest ilośc elementów stosu ? ");
+        y = odczyt.nextInt();
+        stos stos = new stos(y);
+            System.out.println("1=Sumowanie el. stosu");
+            System.out.println("2=Wyrzucenie el. stosu");
+            System.out.println("3=Wypisz el.stosu");
+            System.out.println("4=Rozmiar");
+            
+            x = odczyt.nextInt();
+            switch (x) {
+                case 1:
+                    int E;
+                    System.out.println("Sumowanie elementów stosu");
+                    E = odczyt.nextInt();
+                    stos.push(E);
+                    System.out.println(stos.getMaximumStackSize());
+                    break;
+                case 2:
+                    System.out.println("Wyrzucenie elementów z stosu");
+                    stos.pop();
+                    break;
+                case 3:
+                    stos.pokazstos();
+                    break;
+                case 4:
+                    stos.wielkoscStosu();
+                    break;
+                default:
+                    System.out.println("Podałes złą liczbe");
+                    break;
+            }
+
+        }
     }
-    
-}
--- OOPL5/src/main/java/pl/edu/ur/oopl5/stos.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/stos.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pl.edu.ur.oopl5;
 
 /**
@@ -10,5 +6,47 @@ package pl.edu.ur.oopl5;
  * @author HP
  */
 public class stos {
-    
+ 
+    // Zdefiniowanie tablicy i elementów odpowiedzialnych za wierzchołek.
+    double[] tab;
+    int ws;
+ 
+    // Konstruktor, który zainicjuje wierzchołek i stos o odpowiedniej
+    // wielkości.
+    public stos(int MaxRozmiar) {
+        tab = new double[MaxRozmiar];
+        ws = 0;
+    }
+int getMaximumStackSize() {
+        return ws;
+}
+void push(double E) throws ArrayIndexOutOfBoundsException {
+        if (ws < tab.length) {
+            tab[ws] = E;
+            ws++;
+ 
+        } else {
+            throw new ArrayIndexOutOfBoundsException(
+                    "Przepełnienie stosu");
+        }
+}
+
+double pop() throws IndexOutOfBoundsException {
+        if (ws <= 0) {
+            throw new IndexOutOfBoundsException(
+                    "Stos jest pusty");
+        }
+ 
+        double temp = tab[ws - 1];
+        ws--;
+        return temp;
+}
+ 
+    public void pokazstos(){
+        for (int i = 0; i<tab.length; i++)
+            System.out.println(tab[i]);
+    }
+    public void wielkoscStosu(){
+        System.out.println(ws);
+}
 }
